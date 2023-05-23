001b9422a9d6ce4c403cb211e7e72c61416603d7
kelechi <oparaiskelechi@gmail.com>
2023-05-23 21:21:40 +0100

    add ignore file

--- api/config/config.py
+++ api/config/config.py
@@ -4,6 +4,9 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
+uri = config("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 class Config:
     SECRET_KEY = config('SECRET_KEY', 'secret')
@@ -27,7 +30,9 @@ class TestConfig(Config):
 
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = uri
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = config('DEBUG', cast=bool)
 
 
 config_dict = {
